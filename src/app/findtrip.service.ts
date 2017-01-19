d0956eae02f8f823a5ac21e29abe2aafd7f0ac94
Suresh Kumar Mariappan <suresh.mariappan@klm.com>
2017-01-19 01:24:14 +0100

    added services

---
+++ src/app/findtrip.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { BookingDetails } from './booking.interface';
+import { Observable } from 'rxjs/Rx';
+
+// Import RxJs required methods
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class Findtrip {
+  private apiUrl = 'mock.json';
+
+  constructor(private http: Http) {
+  }
+
+  getTrip(): Observable<BookingDetails> {
+
+    return this.http.get(this.apiUrl)
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+
+  }
+}
