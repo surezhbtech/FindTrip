c31ee4ee16bc892948dc324b079cada277854bf5
Suresh Kumar Mariappan <suresh.mariappan@klm.com>
2017-01-19 01:25:02 +0100

    new gh-pages version

--- src/app/booking-code.ts
+++
@@ -1,4 +0,0 @@
-export interface BookingCode {
-    code:string;
-    lastName:string;
-}
---
+++ styles.58e065928ed8ebd0b582.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([1,2],{393:function(n,o){},394:function(n,o){},927:function(n,o,c){c(393),n.exports=c(394)}},[927]);
+//# sourceMappingURL=styles.58e065928ed8ebd0b582.bundle.map
