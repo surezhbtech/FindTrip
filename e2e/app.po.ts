7ecfdde26441bb4b0ec21a008c20399f11b72896
Suresh Kumar Mariappan <suresh.mariappan@klm.com>
2017-01-22 14:05:32 +0100

    cleared the input fields in the protactor run

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,25 +1,27 @@
-import {browser, element, by} from 'protractor';
+import { browser, element, by } from 'protractor';
 
 export class FindTripPage {
-    navigateTo() {
-        browser.sleep(1000);
-        return browser.get('/');
+  navigateTo() {
+    browser.sleep(1000);
+    return browser.get('/');
 
-    }
+  }
 
-    getParagraphText() {
-        browser.sleep(1000);
-        return element(by.css('app-root h1')).getText();
-    }
+  getParagraphText() {
+    browser.sleep(1000);
+    return element(by.css('app-root h1')).getText();
+  }
 
-    fillBookingCodeAndLastName(bookingCode: string, lastName: string) {
-        element(by.id('bookingcode')).sendKeys(bookingCode);
-        element(by.id('lastname')).sendKeys(lastName);
-        element(by.css('.submit-btn')).click();
-        browser.sleep(1000);
-    }
+  fillBookingCodeAndLastName(bookingCode: string, lastName: string) {
+    element(by.id('bookingcode')).clear();
+    element(by.id('bookingcode')).sendKeys(bookingCode);
+    element(by.id('lastname')).clear();
+    element(by.id('lastname')).sendKeys(lastName);
+    element(by.css('.submit-btn')).click();
+    browser.sleep(1000);
+  }
 
-    getErrorMessage(){
-        return element(by.css('.error')).getText();
-    }
+  getErrorMessage() {
+    return element(by.css('.error')).getText();
+  }
 }
